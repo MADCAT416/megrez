ce445296b8d9bd506535b682c8633cb68e39fb0f
gyhs <42140432+gyhs@users.noreply.github.com>
2019-11-09 21:14:23 +0900

    Update KR_defines.lua

--- common/defines/KR_defines.lua
+++ common/defines/KR_defines.lua
@@ -1,7 +1,7 @@
 -- Kaiserreich defines values
 
-NDefines.NGame.START_DATE = "1987.1.1.12"
-NDefines.NGame.END_DATE = "2020.1.1.1"									-- Vanilla 1949.1.1.1
+NDefines.NGame.START_DATE = "1968.1.1.1"
+NDefines.NGame.END_DATE = "1992.1.1.1"									-- Vanilla 1949.1.1.1
 NDefines.NGame.MAJOR_PARTICIPANTS_FOR_MAJOR_WAR = 20						-- Vanilla is 3
 NDefines.NGame.DECISION_ALERT_TIMEOUT_DAYS = 60								-- Vanilla is 30
 NDefines.NDiplomacy.MAX_TRUST_VALUE = 200									-- Vanilla is 100
